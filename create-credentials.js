a836ce786a4e148ef2e23bd9322fa6585ba0c56f
Cenk Kılıç <cenk@kilic.dev>
2020-04-30 13:27:16 +0200

    forked for npm usecase

--- create-credentials.js
+++ create-credentials.js
@@ -2,4 +2,6 @@ const { PLUGIN_GIT_LOGIN, PLUGIN_GIT_PASSWORD } = process.env;
 
 if (PLUGIN_GIT_LOGIN && PLUGIN_GIT_PASSWORD) {
   console.log(`${encodeURIComponent(PLUGIN_GIT_LOGIN)}:${encodeURIComponent(PLUGIN_GIT_PASSWORD)}`);
+} else {
+  console.log('Credentials are not set properly. Please set settings git_login and git_password')
 }
--- release.sh
+++ release.sh
@@ -2,42 +2,39 @@
 
 export MODE=$PLUGIN_MODE
 
+if [ -z $PLUGIN_GIT_METHOD ]; then
+  echo "Variable git_method must be set in settings."
+  exit 1
+fi
+
 export GIT_AUTHOR_NAME=$PLUGIN_GIT_USER_NAME
 export GIT_AUTHOR_EMAIL=$PLUGIN_GIT_USER_EMAIL
 export GIT_COMMITTER_NAME=$PLUGIN_GIT_USER_NAME
 export GIT_COMMITTER_EMAIL=$PLUGIN_GIT_USER_EMAIL
+export NPM_TOKEN=$PLUGIN_NPM_TOKEN
 
-export GH_TOKEN=$PLUGIN_GITHUB_TOKEN
-export GL_TOKEN=$PLUGIN_GITLAB_TOKEN
-export BB_TOKEN=$PLUGIN_BITBUCKET_TOKEN
-export GIT_CREDENTIALS=$(node /semantic-release/create-credentials.js)
+if [ "$PLUGIN_GIT_METHOD" == "gh" ]; then
+  export GH_TOKEN=$PLUGIN_GITHUB_TOKEN
+elif [ "$PLUGIN_GIT_METHOD" == "gl" ]; then
+  export GL_TOKEN=$PLUGIN_GITLAB_TOKEN
+elif [ "$PLUGIN_GIT_METHOD" == "bb" ]; then
+  export BB_TOKEN=$PLUGIN_BITBUCKET_TOKEN
+elif [ "$PLUGIN_GIT_METHOD" == "cr" ]; then
+  export GIT_CREDENTIALS=$(node /semantic-release/create-credentials.js)
+else
+  echo "Variable git_method must be one of the following: gh (Github), gl (GitLab), bb (BitBucket), cr (Credentials)"
+  exit 1
+fi
 
-cp /semantic-release/.releaserc.json .releaserc
+if [ ! -f .releaserc || "$PLUGIN_USE_LOCAL_RC" == "true" ]; then
+  echo ".releaserc not found using defaults"
+  cp /semantic-release/.releaserc.json .releaserc
+fi
 
 if [ "$MODE" = "predict" ]; then
   echo 'Running semantic release in dry mode...'
 
-  (semantic-release -d || exit 1) | grep 'Published release' | sed -E 's/.*([0-9]+.[0-9]+.[0-9]+)/\1/' > .release-version
+  echo $( (semantic-release -d $PLUGIN_ARGUMENTS || exit 1) | grep 'Published release' | sed -E 's/.*([0-9]+.[0-9]+.[0-9]+)/\1/')
 else
-  semantic-release  || exit 1
-fi
-
-rm .releaserc
-
-FILE=.release-version
-
-if [ -f "$FILE" ]; then
-  VERSION_NUMBER=$(cat $FILE)
-
-  if [ -n "$VERSION_NUMBER" ]; then
-    if [ -n "$PLUGIN_VERSION_FILE" ]; then mv .release-version $PLUGIN_VERSION_FILE; fi
-
-    echo "Successfully released $VERSION_NUMBER"
-  else
-    echo "There is no new version found (file is empty), skipping release..."
-    exit 1
-  fi
-else
-  echo "There is no new version found (no release file generated), skipping release..."
-  exit 1
+  semantic-release $PLUGIN_ARGUMENTS || exit 1
 fi
