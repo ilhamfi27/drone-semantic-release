62f220fdcca0b9780c3514b37e09b83bc6b58370
Cenk Kılıç <cenk@kilic.dev>
2020-04-30 20:19:09 +0200

    changed bullets

--- release.sh
+++ release.sh
@@ -49,7 +49,7 @@ if [ "$MODE" = "predict" ]; then
   echo 'Running semantic release in dry mode...'
   semantic-release -d || exit 1
 else
-  [ $UPDATE_README = 'true' ] && markdown-toc /drone/src/${README_LOCATION:-README.md} -i --no-firsth1 --bullets "-"
+  [ $UPDATE_README = 'true' ] && markdown-toc /drone/src/${README_LOCATION:-README.md} --bullets="-" -i --no-firsth1
   semantic-release $PLUGIN_ARGUMENTS || exit 1
 fi
 
