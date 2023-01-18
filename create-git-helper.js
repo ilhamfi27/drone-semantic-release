97079946d501e63d79d47b8acaa173b7f38fdd60
semantic-release <r.ilhamfadhilah@gmail.com>
2023-01-18 09:54:26 +0700

    fix variable on create-git-helper.js

--- create-git-helper.js
+++ create-git-helper.js
@@ -7,10 +7,10 @@ const {
 
 if (PLUGIN_GIT_HOST && PLUGIN_GIT_HOST_PROTO) {
   console.log(
-    `!f() { echo 'host=${PLUGIN_GIT_HOST}'; echo 'protocol=${PLUGIN_GIT_HOST_PROTO}'; echo 'username=${GIT_LOGIN}'; echo 'password=${GIT_PASSWORD}'; }; f`
+    `!f() { echo 'host=${PLUGIN_GIT_HOST}'; echo 'protocol=${PLUGIN_GIT_HOST_PROTO}'; echo 'username=${PLUGIN_GIT_LOGIN}'; echo 'password=${PLUGIN_GIT_PASSWORD}'; }; f`
   );
 } else {
   console.log(
-    `!f() { echo 'username=${GIT_LOGIN}'; echo 'password=${GIT_PASSWORD}'; }; f`
+    `!f() { echo 'username=${PLUGIN_GIT_LOGIN}'; echo 'password=${PLUGIN_GIT_PASSWORD}'; }; f`
   );
 }
